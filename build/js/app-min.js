6f76f50114aac5a4ccdc3ab8b00e05c2b4af6f92
Alexandr <awdes@yandex.ru>
2023-11-19 14:34:52 +0300

    18 ноября 2023
    14-34

---
+++ build/js/app-min.js
@@ -0,0 +1,2 @@
+window.onload=function(){let e=document.querySelector(".preloader");setTimeout(()=>{e.classList.add("preloader-remove")},250)};
+//# sourceMappingURL=app-min.js.map
---
+++ build/js/app.js
@@ -0,0 +1,9 @@
+//* --------------------------------[jScript]-----------------------------------
+window.onload = function () {
+	let preloader = document.querySelector('.preloader');
+	setTimeout(() => {
+		preloader.classList.add("preloader-remove");
+	}, 250);
+
+};
+//# sourceMappingURL=app.js.map
